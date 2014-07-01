6fcd013175d6e564a05cab85273a9be1d06efcf7
Juan Carlos Garcia Alfaro <juancarlos.garcia.alfaro@gmail.com>
2014-06-30 21:28:26 -0600

    juanca controller agregado para hacer test de como funcionan los branchs

---
+++ TDG_SICACI/Controllers/JuancaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TDG_SICACI.Controllers
+{
+    public class JuancaController : Controller
+    {
+        //
+        // GET: /Juanca/
+
+        public ActionResult Index()
+        {
+            return View();
+            Console.Write("algo");
+            Console.Write("algo mas");
+        }
+
+    }
+}
