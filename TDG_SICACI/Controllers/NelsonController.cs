1ab17fd26872b6ba88d855a91a2baf625759e72a
nelson <nelsonsnake_@hotmail.com>
2014-06-30 21:38:55 -0600

    controlador de prueba nelson

---
+++ TDG_SICACI/Controllers/NelsonController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TDG_SICACI.Controllers
+{
+    public class NelsonController : Controller
+    {
+        //
+        // GET: /Nelson/
+
+        public ActionResult Index()
+        {
+            return View();
+           // Console.Write("prueba");
+        }
+
+    }
+}
