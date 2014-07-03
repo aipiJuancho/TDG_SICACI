cf8b72c0e835d29e04f1c4ce06b9d40daf349956
aipiJuancho <juanmpp@gmail.com>
2014-07-02 23:15:34 -0600

    Clase MVC

--- TDG_SICACI/Controllers/SelfAController.cs
+++ TDG_SICACI/Controllers/SelfAController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JertiFramework.Security;
+using JertiFramework.Controladores;
+using JertiFramework.Interpretes.NotifySystem;
 
 namespace TDG_SICACI.Controllers
 {
@@ -30,6 +33,19 @@ namespace TDG_SICACI.Controllers
             Models.Ej1ViewModel demo = new Models.Ej1ViewModel() { Pregunta = "ola k ase", Respuesta = "nada" };
 
             return View(demo);
+        }
+
+        [HttpPost()]
+        [JFValidarModel()]
+        [JFHandleExceptionMessage(Order=1)]
+        public JsonResult GuardarPregunta(Models.Ej1ViewModel model)
+        {
+            
+            return Json(new
+            {
+                success = true,
+                notify = new JFNotifySystemMessage("Todos los datos estan correctos", "Titulo del Mensaje", icono: JFNotifySystemIcon.NewDoc)
+            });
         }
 
 
--- TDG_SICACI/Models/Ej1ViewModel.cs
+++ TDG_SICACI/Models/Ej1ViewModel.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using JertiFramework.Controls;
 
 namespace TDG_SICACI.Models
 {
+    //http://www.asp.net/mvc/tutorials/mvc-music-store/mvc-music-store-part-6
     public class Ej1ViewModel
     {
         [Required]
+        [JFRejilla(Grid_Label_PC: 6, Grid_Field_PC: 6)]
+        [JFMaxLenght(20)]
+        [RegularExpression("^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$", ErrorMessage= "El correo no es valido")]
+        [Display(Name="Digite la pregunta", Prompt="Por favor, solo correos validos")]
+        [JFOcultarEtiqueta(true)]
         public string Pregunta { get; set; }
 
         [Required]
+        [JFRejilla(Grid_Label_PC: 6, Grid_Field_PC: 6)]
+        [Display(Name = "Digite la respuesta", Prompt="Solo caracteres alfanumericos")]
         public string Respuesta { get; set; }
     }
 }
