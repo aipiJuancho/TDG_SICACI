7f0623f7336ca5930704dfc25124e1a155bd793e
aipiJuancho <juanmpp@gmail.com>
2014-07-04 22:07:02 -0600

    Se ha implemetando el MembershipProvider Se ha implementado el RoleProvider
    Se han ajustado las configuraciones para los nuevos proveedores de cuentas de usuario y roles.
    
    Solo estamos pendiente de enlazar esto a las tablas de la BD.

---
+++ TDG_SICACI/Database/DAL/USERS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDG_SICACI.Database.DAL
+{
+    public partial class SICACI_DAL
+    {
+
+    }
+}
