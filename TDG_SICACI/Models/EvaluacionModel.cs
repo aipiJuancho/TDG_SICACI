eef083a16e2e6653a10c7a293c875d691b6b3ac4
Juan Carlos Garcia Alfaro <juancarlos.garcia.alfaro@gmail.com>
2014-07-09 23:55:42 -0600

    models added

---
+++ TDG_SICACI/Models/EvaluacionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using JertiFramework.Controls;
+
+namespace TDG_SICACI.Models
+{
+    public class EvaluacionModel
+    {
+        // TODO: agregar atributo de las propiedades.
+        public int revision {get; set;}
+        public DateTime fechaCreacion {get; set;}
+        public string comentario {get; set;}
+        public Array respuestas {get; set;}
+        public string idUsuario {get; set;}
+    }
+}
---
+++ TDG_SICACI/Models/UsuarioModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using JertiFramework.Controls;
+
+namespace TDG_SICACI.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Usuario", Prompt = "Username o Nickname")]
+        [JFMaxLenght(16)]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Contraseña", Prompt="Contraseña")]
+        [JFMaxLenght(16)]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        [JFTipoField(JFControlType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class UsuarioModel 
+    {
+        //TODO: agregar atributos a las propiedades
+        public string id { get; set; }
+        public string password { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public int rol { get; set; }
+    }
+
+    public class RolModel 
+    {
+        //TODO: agregar atributos a las propiedades
+        public int id {get; set;}
+        public string tipo {get; set;}
+    }
+}
