9f523b2cac724d48459160b4b37f467c1548b554
Juan Carlos Garcia Alfaro <juancarlos.garcia.alfaro@gmail.com>
2014-07-10 21:51:53 -0600

    agregue los modelos que me faltaron anoche

---
+++ TDG_SICACI/Models/PoliticaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using JertiFramework.Controls;
+
+namespace TDG_SICACI.Models
+{
+    public class PoliticaModel
+    {
+        // TODO: agregar atributo de las propiedades.
+        public string nombre { get; set; }
+        public string descripcion {get; set;}
+        public Array objetivosAsociados{get; set;}
+    }
+}
