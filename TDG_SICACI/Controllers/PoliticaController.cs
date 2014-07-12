5e3e6148085cef5bf15ae5bf6fd11946ed9853f4
Juan Carlos Garcia Alfaro <juancarlos.garcia.alfaro@gmail.com>
2014-07-12 17:04:29 -0600

    controladores en igual condiciones que el anterior, es decir que solo estan con es esqueleto del crud

---
+++ TDG_SICACI/Controllers/PoliticaController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JertiFramework.Security;
+using JertiFramework.Controladores;
+using JertiFramework.Interpretes.NotifySystem;
+
+namespace TDG_SICACI.Controllers
+{
+    public class PoliticaController : Controller
+    {
+        //
+        // GET: /PoliticaModel/
+
+        public ActionResult Index()
+        {
+            //TODO: agregar logica del metodo
+            return View();
+        }
+
+        public ActionResult Agregar()
+        {
+            //TODO: agregar logica del metodo
+            return View();
+        }
+
+        public ActionResult Consultar()
+        {
+            //TODO: agregar logica del metodo
+            return View();
+        }
+
+        public ActionResult Modificar()
+        {
+            //TODO: agregar logica del metodo
+            return View();
+        }
+
+        public ActionResult Eliminar()
+        {
+            //TODO: agregar logica del metodo
+            return View();
+        }
+
+    }
+}
