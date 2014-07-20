c49cfe5d7587cbd52c4f8cc481cdeb27040565c3
Juan Carlos Garcia Alfaro <juancarlos.garcia.alfaro@gmail.com>
2014-07-20 09:35:44 -0600

    vistas de archivo

--- TDG_SICACI/Controllers/ArchivoController.cs
+++ TDG_SICACI/Controllers/ArchivoController.cs
@@ -6,42 +6,93 @@ using System.Web.Mvc;
 using JertiFramework.Security;
 using JertiFramework.Controladores;
 using JertiFramework.Interpretes.NotifySystem;
+using JertiFramework.Interpretes;
+using TDG_SICACI.Database.DAL;
 
 namespace TDG_SICACI.Controllers
 {
-    public class ArchivoController : Controller
+    public class ArchivoController : BaseController
     {
-        //
-        // GET: /Documento/
-
+        [HttpGet()]
         public ActionResult Index()
         {
-            //TODO: agregar logica del metodo
-            return View();
+            List<Models.ArchivoModel> archivos = new List<Models.ArchivoModel>()
+            {
+                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
+                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
+                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
+                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
+                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
+                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
+                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
+                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
+                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" }
+
+            };
+
+            return View(archivos);        
         }
 
+       [HttpGet]
         public ActionResult Agregar()
         {
             //TODO: agregar logica del metodo
             return View();
         }
 
-        public ActionResult Consultar()
+       [HttpPost]
+       [JFValidarModel()]
+       public JsonResult Agregar(Models.ArchivoModel model)
+       {
+           //TODO: agregar logica del metodo
+           return Json(new
+           {
+               success = true,
+               notify = new JFNotifySystemMessage("El archivo se ha agregado correctamente.", titulo: "Completado", permanente: true, icono: JFNotifySystemIcon.Send),
+               redirectURL = "/Archivo/"
+           });
+       }
+
+       [HttpGet()]
+       [JFHandleExceptionMessage(Order = 1)]
+        public ActionResult Consultar(int id)
         {
-            //TODO: agregar logica del metodo
-            return View();
+            Models.ArchivoModel model = new Models.ArchivoModel { id = id, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" };
+            return View(model);
         }
 
-        public ActionResult Modificar()
+        [HttpGet()]
+        [JFHandleExceptionMessage(Order = 1)]
+        public ActionResult Modificar(int id)
         {
-            //TODO: agregar logica del metodo
-            return View();
+            Models.ArchivoModel model = new Models.ArchivoModel { id = id, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" };
+            return View(model);
         }
 
-        public ActionResult Eliminar()
+        [HttpPost]
+        [JFValidarModel()]
+        public JsonResult Modificar(Models.ArchivoModel model)
         {
             //TODO: agregar logica del metodo
-            return View();
+            return Json(new
+            {
+                success = true,
+                notify = new JFNotifySystemMessage("El archivo se ha modificado correctamente.", titulo: "Completado", permanente: true, icono: JFNotifySystemIcon.Send),
+                redirectURL = "/Archivo/"
+            });
+        }
+
+        [HttpGet()]
+        [JFHandleExceptionMessage(Order = 1)]
+        public JsonResult Eliminar(int id)
+        {
+            //TODO: agregar logica del metodo
+            return Json(new
+            {
+                success = true,
+                notify = new JFNotifySystemMessage("El archivo se ha eliminado correctamente.", titulo: "Completado", permanente: true, icono: JFNotifySystemIcon.Send),
+                redirectURL = "/Archivo/"
+            });
         }
     }
 }
