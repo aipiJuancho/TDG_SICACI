b9ce3cf766a3ed2574c1355567771169010341d2
Juan Carlos Garcia Alfaro <juancarlos.garcia.alfaro@gmail.com>
2014-07-20 10:55:25 -0600

    vistas de la norma

--- TDG_SICACI/Controllers/NormaController.cs
+++ TDG_SICACI/Controllers/NormaController.cs
@@ -6,24 +6,50 @@ using System.Web.Mvc;
 using JertiFramework.Security;
 using JertiFramework.Controladores;
 using JertiFramework.Interpretes.NotifySystem;
+using JertiFramework.Interpretes;
+using TDG_SICACI.Database.DAL;
+
 
 namespace TDG_SICACI.Controllers
 {
-    public class NormaController : Controller
+    public class NormaController : BaseController
     {
         //
         // GET: /Norma/
-
+        [HttpGet()]
         public ActionResult Index()
         {
             //TODO: agregar logica del metodo este deberia servir para consultar toda la norma
-            return View();
+            List<Models.NormaModel> norma = new List<Models.NormaModel>()
+            {
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+                new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma"},
+
+            };
+
+            return View(norma);   
         }
 
-        public ActionResult Consultar()
+        [HttpGet()]
+        [JFHandleExceptionMessage(Order = 1)]
+        public ActionResult Consultar(int numeral)
         {
             //TODO: agregar logica del metodo este deberia servir para consultar un numeral de la norma
-            return View();
+            Models.NormaModel model = new Models.NormaModel { numeral = 1, contenido = "texto contenido correspondiente al numeral de la norma" };
+            return View(model);
         }
 
         // los demas metodos no se si deberian poder ocuparse para la norma //
