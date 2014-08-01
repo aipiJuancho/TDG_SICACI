f9f94be800e61c005777ade74fa12c9991fc1428
ifobos <juancarlos.garcia.alfaro@gmail.com>
2014-08-01 00:49:23 -0600

    dummy del gris de archivos hecho, faltan modales

--- TDG_SICACI/Controllers/ArchivoController.cs
+++ TDG_SICACI/Controllers/ArchivoController.cs
@@ -8,94 +8,131 @@ using JertiFramework.Controladores;
 using JertiFramework.Interpretes.NotifySystem;
 using JertiFramework.Interpretes;
 using TDG_SICACI.Database.DAL;
+using System.Net;
+using JertiFramework.Controls;
 
 namespace TDG_SICACI.Controllers
 {
     public class ArchivoController : BaseController
     {
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        #region constants
+        private const string kUserRol = "Administrador";
+        #endregion
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        #region Manage
         [HttpGet()]
         public ActionResult Index()
         {
-            List<Models.ArchivoModel> archivos = new List<Models.ArchivoModel>()
+            if (User.IsInRole(kUserRol))
             {
-                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
-                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
-                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
-                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
-                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
-                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
-                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
-                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" },
-                new Models.ArchivoModel { id = 1, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" }
+                return View();
+            }
+            return new HttpNotFoundResult("No se ha definido la vista para los usuarios no Administradores");
+        }
+        //--------------------------------------------------------------------------------------------------------------//
+        [HttpPost()]
+        [Authorize(Roles = kUserRol)]
+        public JsonResult DataGrid(jfBSGrid_Respond model)
+        {
+            //la data es dummy, por eso no funciona la paginacion correctamente porque como este metodo se ejecuta con cada 
+            //request ajax la data se presenta siempre estatica
 
+            List<Models.Grid_ArchivoViewModel> items = new List<Models.Grid_ArchivoViewModel>()
+            {
+                new Models.Grid_ArchivoViewModel { nombre = "nombre", etiqueta = "etiqueta"},
+                new Models.Grid_ArchivoViewModel { nombre = "nombre", etiqueta = "etiqueta"},
+                new Models.Grid_ArchivoViewModel { nombre = "nombre", etiqueta = "etiqueta"},
+                new Models.Grid_ArchivoViewModel { nombre = "nombre", etiqueta = "etiqueta"},
+                new Models.Grid_ArchivoViewModel { nombre = "nombre", etiqueta = "etiqueta"},
+                new Models.Grid_ArchivoViewModel { nombre = "nombre", etiqueta = "etiqueta"},
+                new Models.Grid_ArchivoViewModel { nombre = "nombre", etiqueta = "etiqueta"},
+                new Models.Grid_ArchivoViewModel { nombre = "nombre", etiqueta = "etiqueta"},
+                new Models.Grid_ArchivoViewModel { nombre = "nombre", etiqueta = "etiqueta"},
+                new Models.Grid_ArchivoViewModel { nombre = "nombre", etiqueta = "etiqueta"}
             };
-
-            return View(archivos);        
-        }
-
-       [HttpGet]
-        public ActionResult Agregar()
-        {
-            //TODO: agregar logica del metodo
-            return View();
-        }
-
-       [HttpPost]
-       [JFValidarModel()]
-       public JsonResult Agregar(Models.ArchivoModel model)
-       {
-           //TODO: agregar logica del metodo
-           //FIXME: no me deja subir el archivo me sale un error y este error si sale bien con ek jf, pero la cosa es que trato de subir un pdf y no me deja =S
-           return Json(new
-           {
-               success = true,
-               notify = new JFNotifySystemMessage("El archivo se ha agregado correctamente.", titulo: "Completado", permanente: true, icono: JFNotifySystemIcon.Send),
-               redirectURL = "/Archivo/"
-           });
-       }
-
-       [HttpGet()]
-       [JFHandleExceptionMessage(Order = 1)]
-        public ActionResult Consultar(int id)
-        {
-            Models.ArchivoModel model = new Models.ArchivoModel { id = id, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" };
-            return View(model);
-        }
-
-        [HttpGet()]
-        [JFHandleExceptionMessage(Order = 1)]
-        public ActionResult Modificar(int id)
-        {
-            Models.ArchivoModel model = new Models.ArchivoModel { id = id, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" };
-            return View(model);
-        }
-
-        [HttpPost]
-        [JFValidarModel()]
-        public JsonResult Modificar(Models.ArchivoModel model)
-        {
-            //TODO: agregar logica del metodo
-            //FIXME: da un error el campo archivo no es requerido pero sigue dando este error "{"success":false,"modelErrors":[{"ID_Object":"documento","MSG_Error":"El campo Archivo no es válido."}]}"
-            return Json(new
+            return Json(new jfBSGrid_ReturnData
             {
-                success = true,
-                notify = new JFNotifySystemMessage("El archivo se ha modificado correctamente.", titulo: "Completado", permanente: true, icono: JFNotifySystemIcon.Send),
-                redirectURL = "/Archivo/"
-            });
-        }
-
-        [HttpGet()]
-        [JFHandleExceptionMessage(Order = 1)]
-        public JsonResult Eliminar(int id)
-        {
-            //TODO: agregar logica del metodo'
-            //FIXME: da un error "{"msg":"Esta solicitud se ha bloqueado porque no se puede revelar información confidencial en sitios web de terceros cuando se utiliza en una solicitud GET. Para permitir solicitudes GET, establezca JsonRequestBehavior en AllowGet."}"
-            return Json(new
-            {
-                success = true,
-                notify = new JFNotifySystemMessage("El archivo se ha eliminado correctamente.", titulo: "Completado", permanente: true, icono: JFNotifySystemIcon.Send),
-                redirectURL = "/Archivo/"
+                total_rows = items.Count(),
+                page_data = items
             }, JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        #region Create
+        [HttpGet()]
+        [JFHandleExceptionMessage(Order = 1)]
+        [Authorize(Roles = kUserRol)]
+        public ActionResult Agregar()
+        {
+            return PartialView();
+        }
+        #endregion
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //   [HttpGet]
+    //    public ActionResult Agregar()
+    //    {
+    //        //TODO: agregar logica del metodo
+    //        return View();
+    //    }
+
+    //   [HttpPost]
+    //   [JFValidarModel()]
+    //   public JsonResult Agregar(Models.ArchivoModel model)
+    //   {
+    //       //TODO: agregar logica del metodo
+    //       //FIXME: no me deja subir el archivo me sale un error y este error si sale bien con ek jf, pero la cosa es que trato de subir un pdf y no me deja =S
+    //       return Json(new
+    //       {
+    //           success = true,
+    //           notify = new JFNotifySystemMessage("El archivo se ha agregado correctamente.", titulo: "Completado", permanente: true, icono: JFNotifySystemIcon.Send),
+    //           redirectURL = "/Archivo/"
+    //       });
+    //   }
+
+    //   [HttpGet()]
+    //   [JFHandleExceptionMessage(Order = 1)]
+    //    public ActionResult Consultar(int id)
+    //    {
+    //        Models.ArchivoModel model = new Models.ArchivoModel { id = id, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" };
+    //        return View(model);
+    //    }
+
+    //    [HttpGet()]
+    //    [JFHandleExceptionMessage(Order = 1)]
+    //    public ActionResult Modificar(int id)
+    //    {
+    //        Models.ArchivoModel model = new Models.ArchivoModel { id = id, etiqueta = "Etiqueta del archivo", nombre = "Nombre del archivo", url = "/Archivos/archivo.pdf" };
+    //        return View(model);
+    //    }
+
+    //    [HttpPost]
+    //    [JFValidarModel()]
+    //    public JsonResult Modificar(Models.ArchivoModel model)
+    //    {
+    //        //TODO: agregar logica del metodo
+    //        //FIXME: da un error el campo archivo no es requerido pero sigue dando este error "{"success":false,"modelErrors":[{"ID_Object":"documento","MSG_Error":"El campo Archivo no es válido."}]}"
+    //        return Json(new
+    //        {
+    //            success = true,
+    //            notify = new JFNotifySystemMessage("El archivo se ha modificado correctamente.", titulo: "Completado", permanente: true, icono: JFNotifySystemIcon.Send),
+    //            redirectURL = "/Archivo/"
+    //        });
+    //    }
+
+    //    [HttpGet()]
+    //    [JFHandleExceptionMessage(Order = 1)]
+    //    public JsonResult Eliminar(int id)
+    //    {
+    //        //TODO: agregar logica del metodo'
+    //        //FIXME: da un error "{"msg":"Esta solicitud se ha bloqueado porque no se puede revelar información confidencial en sitios web de terceros cuando se utiliza en una solicitud GET. Para permitir solicitudes GET, establezca JsonRequestBehavior en AllowGet."}"
+    //        return Json(new
+    //        {
+    //            success = true,
+    //            notify = new JFNotifySystemMessage("El archivo se ha eliminado correctamente.", titulo: "Completado", permanente: true, icono: JFNotifySystemIcon.Send),
+    //            redirectURL = "/Archivo/"
+    //        }, JsonRequestBehavior.AllowGet);
+    //    }
     }
 }
--- TDG_SICACI/Models/ArchivoModel.cs
+++ TDG_SICACI/Models/ArchivoModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using JertiFramework.Controls;
+using System.Web.Mvc;
+using JertiFramework.Database;
 
 namespace TDG_SICACI.Models
 {
@@ -30,5 +32,42 @@ namespace TDG_SICACI.Models
         public HttpPostedFileBase documento { get; set; } 
 
         public string   url         { get; set; }
+    }
+
+    public class Grid_ArchivoViewModel
+    {
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+
+        [Display(Name = "Etiqueta")]
+        public string etiqueta { get; set; }
+    }
+
+    public class Agregar_ArchivoModel
+    {
+        [JFMaxLenght(16)]
+        //[Remote("_validateUser", "Usuario")]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        [Display(Name = "Nombre", Prompt = "Nombre del Archivo")]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [MinLength(4, ErrorMessageResourceName = "LongitudUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        //[RegularExpression(JFRegExpValidators.UserSystem, ErrorMessageResourceName = "CaracteresUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        public string nombre { get; set; }
+
+        [JFMaxLenght(50)]
+        //[Remote("_validateUser", "Usuario")]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        [Display(Name = "Etiqueta", Prompt = "Etiqueta del Archivo")]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [MinLength(4, ErrorMessageResourceName = "LongitudUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        //[RegularExpression(JFRegExpValidators.UserSystem, ErrorMessageResourceName = "CaracteresUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        public string etiqueta { get; set; }
+
+        [Display(Name = "Archivo", Prompt = "Solo archivos *.pdf")]
+        [JFTipoField(JFControlType.File)]
+        [JFFile(1, JFFileAttribute.JFFileExtension.PDF)]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        public HttpPostedFileBase documento { get; set; }
+
     }
 }
