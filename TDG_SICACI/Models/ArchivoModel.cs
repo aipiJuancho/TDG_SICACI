688ef6adc18d8295e693adf6f65edf581f66cfb0
ifobos <juancarlos.garcia.alfaro@gmail.com>
2014-08-11 21:40:01 -0600

    modelo de modificar agregado

--- TDG_SICACI/Models/ArchivoModel.cs
+++ TDG_SICACI/Models/ArchivoModel.cs
@@ -70,10 +70,39 @@ namespace TDG_SICACI.Models
         public HttpPostedFileBase documento { get; set; }
 
     }
+
     public class Consultar_ArchivoModel
     {   
         public string nombre { get; set; }
         public string etiqueta { get; set; }
         public string url { get; set; }
     }
+
+    public class Modificar_ArchivoModel 
+    {
+        [JFMaxLenght(16)]
+        //[Remote("_validateUser", "Usuario")]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        [Display(Name = "Nombre", Prompt = "Nombre del Archivo")]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [MinLength(4, ErrorMessageResourceName = "LongitudUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        //[RegularExpression(JFRegExpValidators.UserSystem, ErrorMessageResourceName = "CaracteresUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        public string nombre { get; set; }
+
+        [JFMaxLenght(50)]
+        //[Remote("_validateUser", "Usuario")]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        [Display(Name = "Etiqueta", Prompt = "Etiqueta del Archivo")]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [MinLength(4, ErrorMessageResourceName = "LongitudUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        //[RegularExpression(JFRegExpValidators.UserSystem, ErrorMessageResourceName = "CaracteresUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        public string etiqueta { get; set; }
+
+        [Display(Name = "Archivo", Prompt = "Solo archivos *.pdf")]
+        [JFTipoField(JFControlType.File)]
+        [JFFile(1, JFFileAttribute.JFFileExtension.PDF)]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        public HttpPostedFileBase documento { get; set; }
+    }
+
 }
