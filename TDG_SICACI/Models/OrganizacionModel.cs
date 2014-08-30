48463ef482d2df0d11ade1723ec041f9873c2133
ifobos <juancarlos.garcia.alfaro@gmail.com>
2014-08-30 11:27:41 -0600

    model to read created

--- TDG_SICACI/Models/OrganizacionModel.cs
+++ TDG_SICACI/Models/OrganizacionModel.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using JertiFramework.Controls;
+using System.Web.Mvc;
+using JertiFramework.Database;
 
 namespace TDG_SICACI.Models
 {
@@ -16,5 +20,17 @@ namespace TDG_SICACI.Models
         public string vision {get; set;}
         public Array valores {get; set;}
         public Array politicas {get; set;}
+    }
+
+    public class Consultar_OrganizacionModel
+    {
+        public string nombre { get; set; }
+        public string logo { get; set; }
+        public string eslogan { get; set; }
+        public string alcance { get; set; }
+        public string mision { get; set; }
+        public string vision { get; set; }
+        public Array valores { get; set; }
+        public Array politicas { get; set; }
     }
 }
