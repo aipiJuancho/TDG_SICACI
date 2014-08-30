66511aa66a080cb6ea74a57a05be78c39bef2ddc
ifobos <juancarlos.garcia.alfaro@gmail.com>
2014-08-30 13:47:54 -0600

    model added

--- TDG_SICACI/Models/OrganizacionModel.cs
+++ TDG_SICACI/Models/OrganizacionModel.cs
@@ -33,4 +33,59 @@ namespace TDG_SICACI.Models
         public Array valores { get; set; }
         public Array politicas { get; set; }
     }
+
+    public class Modificar_organizacionModel 
+    {
+        [JFMaxLenght(100)]
+        //[Remote("_validateUser", "Usuario")]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        [Display(Name = "Nombre", Prompt = "Nombre de la organización")]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [MinLength(4, ErrorMessageResourceName = "LongitudUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        public string nombre { get; set; }
+
+        [Display(Name = "Logo", Prompt = "Solo archivos *.jpg .png")]
+        [JFTipoField(JFControlType.File)]
+        [JFFile(1, JFFileAttribute.JFFileExtension.PDF)]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        public HttpPostedFileBase logo { get; set; }
+
+
+        [JFMaxLenght(200)]
+        //[Remote("_validateUser", "Usuario")]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        [Display(Name = "Eslogan", Prompt = "Eslogan de la organización")]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [MinLength(4, ErrorMessageResourceName = "LongitudUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        public string eslogan { get; set; }
+
+        [JFMaxLenght(500)]
+        //[Remote("_validateUser", "Usuario")]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        [Display(Name = "Alcance", Prompt = "Alcance de la organización")]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [MinLength(4, ErrorMessageResourceName = "LongitudUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        public string alcance { get; set; }
+
+        [JFMaxLenght(500)]
+        //[Remote("_validateUser", "Usuario")]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        [Display(Name = "Misión", Prompt = "Misión de la organización")]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [MinLength(4, ErrorMessageResourceName = "LongitudUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        public string mision { get; set; }
+
+        [JFMaxLenght(500)]
+        //[Remote("_validateUser", "Usuario")]
+        [JFRejilla(Grid_Label_PC: 3, Grid_Field_PC: 9)]
+        [Display(Name = "Visión", Prompt = "Visión de la organización")]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ErrorMessages))]
+        [MinLength(4, ErrorMessageResourceName = "LongitudUser", ErrorMessageResourceType = typeof(ErrorMessages))]
+        public string vision { get; set; }
+
+        public Array valores { get; set; }
+        
+        public Array politicas { get; set; }
+    }
+
 }
